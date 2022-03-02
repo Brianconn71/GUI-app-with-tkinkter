d8ccfa4aa4a716e3a6a9a4bdc6cd33f766ad8dbd
Brian Connolly <Brian.connolly71@gmail.com>
2022-03-02 18:19:02 +0000

    success in extracting pdf content to the terminal

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import PyPDF2
 from PIL import Image, ImageTk
+from tkinter.filedialog import askopenfile
 
 
 # beginning of interface
@@ -23,11 +24,27 @@ instructions = tk.Label(root, text="Select a PDF file on your computer to extrac
 instructions.grid(columnspan=3, column=0, row=1)
 
 
+#functrion for button
+def open_file():
+    browse_text.set("loading...")
+    file = askopenfile(parent=root, mode='rb', title='Choose a file', filetype=[("Pdf file", "*.pdf")])
+    if file:
+        read_pdf = PyPDF2.PdfFileReader(file)
+        page = read_pdf.getPage(0)
+        page_content = page.extractText()
+        
+        # Text widget
+
+
 #browse button
 browse_text = tk.StringVar()
-browse_btn = tk.Button(root, textvariable=browse_text, font="Montserrat", bg="#20bebe", fg="white", height=2)
+browse_btn = tk.Button(root, textvariable=browse_text, command=lambda:open_file(), font="Montserrat", bg="#20bebe", fg="white", height=2, width=15)
 browse_text.set("Browse")
 browse_btn.grid(column=1, row=2)
+
+
+canvas = tk.Canvas(root, width=600, height=250)
+canvas.grid(columnspan=3)
 
 
 root.mainloop()
