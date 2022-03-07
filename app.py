674bcd05240bb37b68685727df5c00494081c1e4
Brian Connolly <Brian.connolly71@gmail.com>
2022-03-07 18:04:16 +0000

    add: finished off basic functionality og software

--- app.py
+++ app.py
@@ -34,6 +34,13 @@ def open_file():
         page_content = page.extractText()
         
         # Text widget.
+        text_box = tk.Text(root, height=10, width=50, padx=15, pady=15)
+        text_box.insert(1.0, page_content)
+        text_box.tag_configure("center", justify="center")
+        text_box.tag_add("center", 1.0, "end")
+        text_box.grid(column=1, row=3)
+        
+        browse_text.set("Browse")
 
 
 #browse button
